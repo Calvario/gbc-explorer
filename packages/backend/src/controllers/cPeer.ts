d821a3f9276cd4f3b5d7df7dbe5f695684bfc711
Steve Calvario <calvario.steve@gmail.com>
2020-10-04 18:36:43 +0000

    Make clear the GPLv3

--- packages/backend/src/controllers/cPeer.ts
+++ packages/backend/src/controllers/cPeer.ts
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2020 Steve Calvário
+ *
+ * This file is part of GBC Explorer, a web multi-coin blockchain explorer.
+ *
+ * GBC Explorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * GBC Explorer is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * GBC Explorer. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import axios from 'axios';
 import { RPCClient } from "rpc-bitcoin";
 import { getManager, EntityManager } from "typeorm";
